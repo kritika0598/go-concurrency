fabbcb3b7a498884fb22f5499948fe0b3b360d31
Kritika Singhal <kritika@Kritikas-MacBook-Air.local>
2023-11-16 23:12:59 +0530

    Add example for race condition

---
+++ race/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var count int32
+	wg.Add(5)
+	go func() {
+		defer wg.Done()
+		count += 10
+	}()
+	go func() {
+		defer wg.Done()
+		count -= 15
+	}()
+	go func() {
+		defer wg.Done()
+		count++
+	}()
+	go func() {
+		defer wg.Done()
+		count = 0
+	}()
+	go func() {
+		defer wg.Done()
+		count = 100
+	}()
+	wg.Wait()
+	fmt.Println("count: ", count)
+}
